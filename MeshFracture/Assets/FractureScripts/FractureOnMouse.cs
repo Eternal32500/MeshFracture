a20eac0f2626ba94fe83d819affd195495d1302b
Eternal32500 <eliott.blesz@gmail.com>
2026-02-03 18:15:04 +0100

    add: FractureOnPoint functionnal

--- MeshFracture/Assets/FractureScripts/FractureOnMouse.cs
+++ MeshFracture/Assets/FractureScripts/FractureOnMouse.cs
@@ -9,7 +9,6 @@ public class FractureOnMouse : Fracture
             return;
 
         Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
 
         if(hit.collider == null)
